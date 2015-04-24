a018b36f936c41a3919d0890e26e854300dd736c
Sean Cross <xobs@kosagi.com>
2015-04-24 13:53:22 +0800

    orchard: Add accelerometer definitions
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/orchard.h
+++ orchard/orchard.h
@@ -3,12 +3,15 @@
 
 extern const char *gitversion;
 
-#define ORCHARD_OS_VERSION_MAJOR 1
-#define ORCHARD_OS_VERSION_MINOR 0
+#define ORCHARD_OS_VERSION_MAJOR      1
+#define ORCHARD_OS_VERSION_MINOR      0
 
-#define serialDriver (&SD1)
-#define stream_driver ((BaseSequentialStream *)serialDriver)
+#define serialDriver                  (&SD1)
+#define stream_driver                 ((BaseSequentialStream *)serialDriver)
 extern void *stream;
+
+#define i2cDriver                     (&I2CD2)
+#define accelAddr                     0x1c
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))

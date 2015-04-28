d57b943285af66537b638b975ac1f6bfb1dc789b
Sean Cross <xobs@kosagi.com>
2015-04-28 19:41:08 +0800

    radio: Use new SPI radio definitions
    This unifies the radioStart() call with other xStart() calls.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/radio.c
+++ orchard/radio.c
@@ -8,27 +8,6 @@
 
 static SPIDriver *driver;
 
-extern void spi_xmit_byte_sync(uint8_t byte);
-extern uint8_t spi_recv_byte_sync(void);
-
-static void my_spiReceive(SPIDriver *driver, int count, uint8_t *bfr) {
-
-  int i;
-
-  (void)driver;
-  for (i = 0; i < count; i++)
-    bfr[i] = spi_recv_byte_sync();
-}
-
-static void my_spiSend(SPIDriver *driver, int count, const uint8_t *bfr) {
-
-  int i;
-
-  (void)driver;
-  for (i = 0; i < count; i++)
-    spi_xmit_byte_sync(bfr[i]);
-}
-
 static const SPIConfig spi_config = {
   NULL,
   /* HW dependent part.*/
@@ -36,9 +15,9 @@ static const SPIConfig spi_config = {
   0,
 };
 
-void radioStart(void) {
+void radioStart(SPIDriver *spip) {
 
-  driver = &SPID1;
+  driver = spip;
   spiStart(driver, &spi_config);
 }
 
@@ -46,16 +25,19 @@ void radioStart(void) {
 
 uint8_t radioRead(uint8_t addr) {
 
-  uint8_t val;
-  uint8_t dummy = 0xff;
+  uint8_t val[10];
+  unsigned int i;
 
   spiSelect(driver);
-  my_spiSend(driver, 1, &addr);
-  my_spiSend(driver, 1, &dummy);
-  my_spiReceive(driver, 1, &val);
+  spiSend(driver, 1, &addr);
+  spiReceive(driver, sizeof(val), val);
+  chprintf(stream, "Debug %d bytes:\r\n", sizeof(val));
+  for (i = 0; i < sizeof(val); i++)
+    chprintf(stream, "    %02x: %02x\r\n", addr + i, val[i]);
+
   spiUnselect(driver);
 
-  return val;
+  return val[0];
 }
 
 void radioWrite(uint8_t addr, uint8_t val) {
@@ -63,6 +45,16 @@ void radioWrite(uint8_t addr, uint8_t val) {
   uint8_t buf[2] = {addr | 0x80, val};
 
   spiSelect(driver);
-  my_spiSend(driver, 2, buf);
+  spiSend(driver, 2, buf);
   spiUnselect(driver);
+}
+
+int radioDump(uint8_t addr, void *bfr, int count) {
+
+  spiSelect(driver);
+  spiSend(driver, 1, &addr);
+  spiReceive(driver, count, bfr);
+  spiUnselect(driver);
+
+  return 0;
 }
--- orchard/radio.h
+++ orchard/radio.h
@@ -1,8 +1,9 @@
 #ifndef __ORCHARD_RADIO_H__
 #define __ORCHARD_RADIO_H__
 
-void radioStart(void);
+void radioStart(SPIDriver *spip);
 uint8_t radioRead(uint8_t addr);
 void radioWrite(uint8_t addr, uint8_t val);
+int radioDump(uint8_t addr, void *bfr, int count);
 
 #endif /* __ORCHARD_RADIO_H__ */

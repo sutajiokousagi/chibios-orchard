e11e28333b08c290f271d06c10af8447ee9bd8b1
Sean Cross <xobs@kosagi.com>
2015-04-29 17:44:17 +0800

    orchard: Add more I2C address definitions
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/orchard.h
+++ orchard/orchard.h
@@ -13,6 +13,9 @@ extern void *stream;
 #define i2cDriver                     (&I2CD2)
 #define accelAddr                     0x1c
 #define gpioxAddr                     0x43
+#define ggAddr                        0x55
+#define touchAddr                     0x5a
+#define chargerAddr                   0x6b
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))

578e75da01463f930726fda579504a5f592279ed
Sean Cross <xobs@kosagi.com>
2015-04-30 19:33:31 +0800

    accel: Add register definitions, freefall detect
    Freefall detection now functions correctly, and emits an event.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/accel.h
+++ orchard/accel.h
@@ -9,5 +9,13 @@ struct accel_data {
 
 void accelStart(I2CDriver *driver);
 msg_t accelPoll(struct accel_data *data);
+void accelEnableFreefall(int sensitivity, int debounce);
+void accelDisableFreefall(void);
+
+extern event_source_t accel_x_axis_pulse;
+extern event_source_t accel_y_axis_pulse;
+extern event_source_t accel_z_axis_pulse;
+extern event_source_t accel_landscape_portrait;
+extern event_source_t accel_freefall;
 
 #endif /* __ORCHARD_ACCEL_H__ */

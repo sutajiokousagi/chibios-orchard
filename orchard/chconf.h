4be8f73b874bd5b3faa40a18bccba00edcf86df2
Sean Cross <xobs@kosagi.com>
2015-05-05 16:43:10 +0800

    chconf: Break into debugger on error
    When something breaks, dump the user into the debugger.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/chconf.h
+++ orchard/chconf.h
@@ -484,7 +484,8 @@
  *          the system is halted.
  */
 #define CH_CFG_SYSTEM_HALT_HOOK(reason) {                                   \
-  /* System halt code here.*/                                               \
+  /* Break into the debugger on halt.*/                                     \
+  asm("bkpt #0");                                                           \
 }
 
 /** @} */

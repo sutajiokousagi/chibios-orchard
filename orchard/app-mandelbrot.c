75b1eb4036a0e9fce034d6fa7bbcf6011d974bab
Sean Cross <xobs@kosagi.com>
2015-05-12 11:27:17 +0800

    app-mandelbrot: Watch for thread exiting
    Check to see if we're supposed to exit, and quit if so.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/app-mandelbrot.c
+++ orchard/app-mandelbrot.c
@@ -29,7 +29,7 @@
 
 #include "orchard-app.h"
 
-void mandelbrot(float x1, float y1, float x2, float y2) {
+static void mandelbrot(float x1, float y1, float x2, float y2) {
 	unsigned int i,j, width, height;
 	uint16_t iter;
 	color_t color;
@@ -44,26 +44,26 @@ void mandelbrot(float x1, float y1, float x2, float y2) {
 	fwidth = width;
 	fheight = height;
 	
-	for(i = 0; i < width; i++) {
-		for(j = 0; j < height; j++) {
+	for (i = 0; i < width; i++) {
+		for (j = 0; j < height; j++) {
 			float cy = j * sy / fheight + y1;
 			float cx = i * sx / fwidth + x1;
-			float x=0.0f, y=0.0f, xx=0.0f, yy=0.0f;
-			for(iter=0; iter <= MAX && xx+yy<4.0f; iter++) {
+			float x = 0.0f, y = 0.0f, xx = 0.0f, yy = 0.0f;
+			for (iter = 0; iter <= MAX && xx + yy < 4.0f; iter++) {
 				xx = x*x;
 				yy = y*y;
-				y = 2.0f*x*y + cy;
+				y = 2.0f * x * y + cy;
 				x = xx - yy + cx;
 			}
-			//color = ((iter << 8) | (iter&0xFF));
       if (iter > 64)
         color = White;
       else
         color = Black;
-//			color = RGB2COLOR(iter<<7, iter<<4, iter);
 			gdispDrawPixel(i, j, color);
 		}
-      gdispFlush();
+    gdispFlush();
+    if (chThdShouldTerminateX())
+      return;
 	}
 }
 
@@ -79,7 +79,7 @@ static void mandelbrot_start(OrchardAppContext *context) {
 	cx = -0.086f;
 	cy = 0.85f;
 
-	while(TRUE) {
+	while (!chThdShouldTerminateX()) {
 		mandelbrot(-2.0f*zoom+cx, -1.5f*zoom+cy, 2.0f*zoom+cx, 1.5f*zoom+cy);
 
 		zoom *= 0.7f;

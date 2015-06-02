5e6f0ef73cf2a24a1db97ead754be7daa05a1e7b
bunnie <bunnie@kosagi.com>
2015-06-02 15:41:26 +0800

    reduce orchard-shell stack from 1024 to 768 bytes.
    general rule: don't do big stuff in the shell.

--- orchard/orchard-shell.c
+++ orchard/orchard-shell.c
@@ -30,7 +30,7 @@ static const SerialConfig serialConfig = {
 };
 
 static thread_t *shell_tp = NULL;
-static THD_WORKING_AREA(waShellThread, 1024);
+static THD_WORKING_AREA(waShellThread, 768);
 
 void orchardShellInit(void)
 {

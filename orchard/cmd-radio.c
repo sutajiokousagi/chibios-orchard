8d7adc99bbee8832799e0227d1ef54080b95c0cb
Sean Cross <xobs@kosagi.com>
2015-06-03 14:34:00 +0800

    cmd-radio: Use new hex.c utilities
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/cmd-radio.c
+++ orchard/cmd-radio.c
@@ -27,47 +27,7 @@
 #include "orchard-shell.h"
 
 #include "radio.h"
-
-static inline int isprint(int c)
-{
-  return c > 32 && c < 127;
-}
-
-int print_hex_offset(BaseSequentialStream *chp,
-                     const void *block, int count, int offset, uint32_t start)
-{
-
-  int byte;
-  const uint8_t *b = block;
-
-  count += offset;
-  b -= offset;
-  for ( ; offset < count; offset += 16) {
-    chprintf(chp, "%08x", start + offset);
-
-    for (byte = 0; byte < 16; byte++) {
-      if (byte == 8)
-        chprintf(chp, " ");
-      chprintf(chp, " ");
-      if (offset + byte < count)
-        chprintf(chp, "%02x", b[offset + byte] & 0xff);
-      else
-        chprintf(chp, "  ");
-    }
-
-    chprintf(chp, "  |");
-    for (byte = 0; byte < 16 && byte + offset < count; byte++)
-      chprintf(chp, "%c", isprint(b[offset + byte]) ?  b[offset + byte] : '.');
-    chprintf(chp, "|\r\n");
-  }
-  return 0;
-}
-
-int print_hex(BaseSequentialStream *chp,
-              const void *block, int count, uint32_t start)
-{
-  return print_hex_offset(chp, block, count, 0, start);
-}
+#include "hex.h"
 
 static void radio_get(BaseSequentialStream *chp, int argc, char *argv[]) {
 

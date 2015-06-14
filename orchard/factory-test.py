658abe2d0c562e110ee04a591316fef1b21cdfc7
Sean Cross <xobs@kosagi.com>
2015-06-14 16:54:55 +0800

    factory-test: Add factory test with pane
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/factory-test.py
+++ orchard/factory-test.py
@@ -1,11 +1,37 @@
 """
 Run gdb with:
 
-gdb -ex 'source test.py' -ex 'target remote localhost:3333' build/orchard.elf
+gdb -ex 'source factory-test.py' -ex 'target remote localhost:3333' -ex 'break orchardShellRestart' -ex 'continue' build/orchard.elf
 """
 import gdb
 import sys
 import gtk
+
+def run_test(test_name, test_type):
+    tests = list_tests()
+    test_index = tests.index(test_name)
+    testidx = "first_test[" + str(test_index) + "]"
+    cmd = "call "+testidx+".test_function("+testidx+", " + str(test_type) + ")"
+
+    result = gdb.execute(cmd, False, True)
+    for line in result.split("\n"):
+        if line.startswith("$"):
+#            print("Evaluating line: " + line)
+#            return gdb.parse_and_eval(line.split(' ')[0])
+            return line.split(' ')[2].replace("orchardResult", "")
+    return None
+
+def list_tests():
+    tests = []
+    i = 0
+    while True:
+        val = gdb.parse_and_eval("first_test[" + str(i) + "].test_name")
+        if int(val.referenced_value()) == 0:
+            break
+        else:
+            tests.append(val.string())
+            i = i + 1
+    return tests
 
 class OrchardCall(gdb.Command):
     def __init__(self):
@@ -16,58 +42,89 @@ class OrchardCall(gdb.Command):
             )
     def invoke(self, argument, from_tty):
         print("Greetings again from Python")
-        result = gdb.execute("call captouchRead()", False, True)
-        print(">> " + result + " <<")
-        convenience_arg = result.split(' ')[0]
-        print("Convenience arg is: " + convenience_arg)
-
-        val = gdb.parse_and_eval(convenience_arg)
-        print("Val type code is: " + val.type.name)
+        list_tests()
 
 OrchardCall()
 
 from threading import Thread
 import gtk
 
-def printit ():
-    print "Hello hacker"
-    print(">>" + gdb.execute("call captouchRead()", False, True) + "<<")
-
-class TestGtkThread (Thread):
-    def destroy (self, *args):
+class TestGtkThread(Thread):
+    def destroy(self, *args):
         self.window.hide()
 
-    def hello (self, *args):
-        gdb.post_event(printit)
+    def runtest(self, button, arg):
+        testname = button.get_label()
 
-    def run (self):
+        self.result_label.set_label("Testing " + testname + "...")
+
+        # Force a redraw
+        gtk.main_iteration()
+
+        result = run_test(testname, arg)
+        self.result_label.set_label("Testing " + testname + ": " + result)
+
+    def run(self):
         gtk.gdk.threads_init()
+
+        tests = list_tests()
 
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("destroy", self.destroy)
         self.window.set_border_width(10)
+        self.window.set_title("Orchard Tests")
 
-        button = gtk.Button("Hello World")
-        # connects the 'hello' function to the clicked signal from the button
-        button.connect("clicked", self.hello)
-        self.window.add(button)
-        button.show()
+        table = gtk.Table(4, len(tests) + 2, False)
+        row = 0
+        self.window.add(table)
+
+        table.attach(gtk.Label("Poweron"),       0, 1, row, row + 1)
+        table.attach(gtk.Label("Trivial"),       1, 2, row, row + 1)
+        table.attach(gtk.Label("Comprehensive"), 2, 3, row, row + 1)
+        table.attach(gtk.Label("Interactive"),   3, 4, row, row + 1)
+        row = row + 1
+
+        for test in tests:
+            button = gtk.Button(test)
+            button.connect("clicked", self.runtest, 0)
+            table.attach(button, 0, 1, row, row + 1)
+
+            button = gtk.Button(test)
+            button.connect("clicked", self.runtest, 1)
+            table.attach(button, 1, 2, row, row + 1)
+
+            button = gtk.Button(test)
+            button.connect("clicked", self.runtest, 2)
+            table.attach(button, 2, 3, row, row + 1)
+
+            button = gtk.Button(test)
+            button.connect("clicked", self.runtest, 3)
+            table.attach(button, 3, 4, row, row + 1)
+            row = row + 1
+
+        self.result_label = gtk.Label("")
+        table.attach(self.result_label, 0, 4, row, row + 1)
+        row = row + 1
 
         self.window.show_all()
         gtk.main()
 
-class TestGtk (gdb.Command):
+class TestPane(gdb.Command):
     def __init__ (self):
-        super (TestGtk, self).__init__ ("testgtk", gdb.COMMAND_NONE,
-                                         gdb.COMPLETE_NONE)
+        super(TestPane, self).__init__ ("testpane",
+                                        gdb.COMMAND_NONE,
+                                        gdb.COMPLETE_NONE)
         self.init = False
 
-    def invoke (self, arg, from_tty):
+    def invoke(self, arg, from_tty):
         self.dont_repeat()
+        gdb.execute("set pagination off")
+#        gdb.execute("mon reset halt")
+#        gdb.execute("continue")
         if not self.init:
             self.init = True
             v = TestGtkThread()
-            v.setDaemon (True)
-            v.start ()
+            v.setDaemon(True)
+            v.start()
 
-TestGtk()
+TestPane()

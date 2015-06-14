8246af2cbb0831d846098aa8ce9b08db6ac30d7d
Sean Cross <xobs@kosagi.com>
2015-06-14 13:07:39 +0800

    app-testmode: Fix up warnings
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/app-testmode.c
+++ orchard/app-testmode.c
@@ -3,49 +3,27 @@
 
 #include "radio.h"
 
-static uint32_t testmode_init(OrchardAppContext *context) {
-
-  (void)context;
-  return 0;
-}
-
 static void testmode_start(OrchardAppContext *context) {
   (void)context;
   
   orchardTestPrompt("Testing...", "", 0);
 }
 
-static void testmode_event(OrchardAppContext *context, const OrchardAppEvent *event) {
-
-  (void)context;
-  // dummy app that does nothing with events sent to it.
-  // tests will create events and having any other app running could interfere with tests
-}
-
-static void testmode_exit(OrchardAppContext *context) {
-
-  (void)context;
-}
-
-orchard_app("~testmode", testmode_init, testmode_start, testmode_event, testmode_exit);
+orchard_app("~testmode", NULL, testmode_start, NULL, NULL);
 
 
 static uint32_t test_rxdat;
 static void test_peer_handler(uint8_t type, uint8_t src, uint8_t dst,
-			       uint8_t length, void *data) {
+                              uint8_t length, void *data) {
 
   (void)length;
   (void)type;
+  (void)src;
+  (void)dst;
 
   test_rxdat = *((uint32_t *) data);
-    
+
   chEvtBroadcast(&radio_app);
-}
-
-static uint32_t testpeer_init(OrchardAppContext *context) {
-
-  (void)context;
-  return 0;
 }
 
 static void testpeer_start(OrchardAppContext *context) {
@@ -56,21 +34,18 @@ static void testpeer_start(OrchardAppContext *context) {
   orchardTestPrompt("Now a test peer", "reboot to stop.", 0);
 }
 
-static void testpeer_event(OrchardAppContext *context, const OrchardAppEvent *event) {
+static void testpeer_event(OrchardAppContext *context,
+                           const OrchardAppEvent *event) {
   (void)context;
   char datstr[16];
   
   if(event->type == radioEvent) {
     chprintf(stream, "received %08x, rebroadcasting...\n\r", test_rxdat);
-    chsnprintf(datstr, sizeof(datstr), "%08x", test_rxdat );
+    chsnprintf(datstr, sizeof(datstr), "%08x", test_rxdat);
     orchardTestPrompt("received:", datstr, 0);
-    radioSend(radioDriver, RADIO_BROADCAST_ADDRESS, RADIO_TYPE_PEER_TO_DUT, 4, &test_rxdat);
+    radioSend(radioDriver, RADIO_BROADCAST_ADDRESS, RADIO_TYPE_PEER_TO_DUT,
+              4, &test_rxdat);
   }
 }
 
-static void testpeer_exit(OrchardAppContext *context) {
-
-  (void)context;
-}
-
-orchard_app("~testpeer", testpeer_init, testpeer_start, testpeer_event, testpeer_exit);
+orchard_app("~testpeer", NULL, testpeer_start, testpeer_event, NULL);

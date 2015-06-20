88d51c0419280c1218ec9283863b468d7a360b39
bunnie <bunnie@kosagi.com>
2015-06-21 03:01:16 +0800

    add debug functions

--- orchard/cmd-friends.c
+++ orchard/cmd-friends.c
@@ -4,6 +4,12 @@
 
 #include "orchard-shell.h"
 #include "orchard-app.h"
+#include "genes.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#define FRIENDS_INIT_CREDIT 4
 
 void cmd_friendlist(BaseSequentialStream *chp, int argc, char *argv[])
 {
@@ -15,9 +21,43 @@ void cmd_friendlist(BaseSequentialStream *chp, int argc, char *argv[])
   friends = friendsGet();
   for(i = 0; i < MAX_FRIENDS; i++) {
     if( friends[i] != NULL ) {
-      chprintf(chp, "%d %s\n\r", (int) friends[i][0], &(friends[i][1]));
+      chprintf(chp, "%d: %d %s\n\r", i, (int) friends[i][0], &(friends[i][1]));
     }
   }
 }
 
 orchard_command("friendlist", cmd_friendlist);
+
+void cmd_friendadd(BaseSequentialStream *chp, int argc, char *argv[]) {
+  char **friends;
+  uint32_t i;
+
+  if (argc != 2) {
+    chprintf(chp, "Usage: friendadd <index> <name>\r\n");
+    return;
+  }
+  friends = (char **)friendsGet();
+  
+  i = strtoul(argv[0], NULL, 0);
+  if( i > MAX_FRIENDS )
+    i = MAX_FRIENDS;
+   
+  if( friends[i] != NULL ) {
+    chprintf(chp, "Index used. Incrementing %s instead.\n\r", &(friends[i][1]));
+    friends[i][0]++;
+  } else {
+    friends[i] = (char *) chHeapAlloc(NULL, GENE_NAMELENGTH + 2); // space for NULL + metric byte
+    friends[i][0] = FRIENDS_INIT_CREDIT;
+    strncpy(&(friends[i][1]), argv[1], GENE_NAMELENGTH);
+  }
+}
+orchard_command("friendadd", cmd_friendadd);
+
+void cmd_friendping(BaseSequentialStream *chp, int argc, char *argv[]) {
+  (void) chp;
+  (void) argc;
+  (void) argv;
+
+  chEvtBroadcast(&radio_app);
+}
+orchard_command("friendping", cmd_friendping);

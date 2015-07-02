c9c3ab4a995ba3a1c869130444b2131324d905f1
Sean Cross <xobs@kosagi.com>
2015-07-02 08:15:21 +0000

    factory-test: Set default verbosity to 1
    We want /some/ information to be present by default.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/factory-test.c
+++ orchard/factory-test.c
@@ -977,6 +977,7 @@ int main(int argc, char **argv) {
   f.cfg.button_gpio = -1;
   f.cfg.do_program = 1;
   f.cfg.do_tests = 1;
+  f.cfg.verbose = 1;
 
   signal(SIGCHLD, handle_sigchld);
   signal(SIGPIPE, handle_sigpipe);
